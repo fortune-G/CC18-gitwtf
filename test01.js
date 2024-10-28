e325ae70d3f7f0cab256558c9a30c669b65a7f08
forture-G <gorakod1234@gmail.com>
2024-10-28 14:13:17 +0700

    change to The server

--- test01.js
+++ test01.js
@@ -1,5 +1,10 @@
-function incresement(num) {
-    
+const server = require('express')
+const app = express() 
+require('dotenv').config() 
 
-    
-}
+
+
+const port = process.env.PORT || 8000 
+
+
+app.listen(port,()=>console.log("GOGO",port))
